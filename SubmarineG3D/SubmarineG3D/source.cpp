8b0cd62d91318d4534b1b570b0b9c90b61556866
RaresMoroie <rares.moroie@student.unitbv.ro>
2023-12-24 11:47:51 +0200

    Am configurat lightVAO

--- SubmarineG3D/SubmarineG3D/source.cpp
+++ SubmarineG3D/SubmarineG3D/source.cpp
@@ -23,6 +23,7 @@
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 void renderFloor();
+void renderScene();
 
 
 
@@ -144,4 +145,11 @@ void renderScene()
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
+
+	glGenVertexArrays(2, &lightVAO);
+	glBindVertexArray(lightVAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
 }
