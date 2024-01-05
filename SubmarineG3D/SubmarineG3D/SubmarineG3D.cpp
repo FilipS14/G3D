4637ce8a3c75d69f61933cbafa90e6bf002ad04d
Geo_Dude27 <rares.vilcu@student.unitbv.ro>
2024-01-05 22:48:21 +0200

    Initializarea camerei si miscarea ei

--- SubmarineG3D/SubmarineG3D/SubmarineG3D.cpp
+++ SubmarineG3D/SubmarineG3D/SubmarineG3D.cpp
@@ -1,17 +1,219 @@
-#include <iostream>
 
-int main()
+
+
+
+
+
+
+
+
+
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+enum ECameraMovementType
 {
-    std::cout << "Hello World!\n";
+	UNKNOWN,
+	FORWARD,
+	BACKWARD,
+	LEFT,
+	RIGHT,
+	UP,
+	DOWN
+};
+
+class Camera
+{
+private:
+	const float zNEAR = 0.1f;
+	const float zFAR = 500.f;
+	const float YAW = -90.0f;
+	const float PITCH = 0.0f;
+	const float FOV = 45.0f;
+	glm::vec3 startPosition;
+
+public:
+	Camera(const int width, const int height, const glm::vec3& position)
+	{
+		startPosition = position;
+		Set(width, height, position);
+	}
+
+	void Set(const int width, const int height, const glm::vec3& position)
+	{
+		this->isPerspective = true;
+		this->yaw = YAW;
+		this->pitch = PITCH;
+
+		this->FoVy = FOV;
+		this->width = width;
+		this->height = height;
+		this->zNear = zNEAR;
+		this->zFar = zFAR;
+
+		this->worldUp = glm::vec3(0, 1, 0);
+		this->position = position;
+
+		lastX = width / 2.0f;
+		lastY = height / 2.0f;
+		bFirstMouseMove = true;
+
+		UpdateCameraVectors();
+	}
+
+	void Reset(const int width, const int height)
+	{
+		Set(width, height, startPosition);
+	}
+
+	void Reshape(int windowWidth, int windowHeight)
+	{
+		width = windowWidth;
+		height = windowHeight;
+
+		glViewport(0, 0, windowWidth, windowHeight);
+	}
+
+	const glm::mat4 GetViewMatrix() const
+	{
+		return glm::lookAt(position, position + forward, up);
+	}
+
+	const glm::vec3 GetPosition() const
+	{
+		return position;
+	}
+
+	const glm::mat4 GetProjectionMatrix() const
+	{
+		glm::mat4 Proj = glm::mat4(1);
+		if (isPerspective) {
+			float aspectRatio = ((float)(width)) / height;
+			Proj = glm::perspective(glm::radians(FoVy), aspectRatio, zNear, zFar);
+		}
+		else {
+			float scaleFactor = 2000.f;
+			Proj = glm::ortho<float>(
+				-width / scaleFactor, width / scaleFactor,
+				-height / scaleFactor, height / scaleFactor, -zFar, zFar);
+		}
+		return Proj;
+	}
+
+	void ProcessKeyboard(ECameraMovementType direction, float deltaTime)
+	{
+		float velocity = (float)(cameraSpeedFactor * deltaTime);
+		switch (direction) {
+		case ECameraMovementType::FORWARD:
+			position += forward * velocity;
+			break;
+		case ECameraMovementType::BACKWARD:
+			position -= forward * velocity;
+			break;
+		case ECameraMovementType::LEFT:
+			position -= right * velocity;
+			break;
+		case ECameraMovementType::RIGHT:
+			position += right * velocity;
+			break;
+		case ECameraMovementType::UP:
+			position += up * velocity;
+			break;
+		case ECameraMovementType::DOWN:
+			position -= up * velocity;
+			break;
+		}
+	}
+
+	void MouseControl(float xPos, float yPos)
+	{
+		if (bFirstMouseMove) {
+			lastX = xPos;
+			lastY = yPos;
+			bFirstMouseMove = false;
+		}
+
+		float xChange = xPos - lastX;
+		float yChange = lastY - yPos;
+		lastX = xPos;
+		lastY = yPos;
+
+		if (fabs(xChange) <= 1e-6 && fabs(yChange) <= 1e-6) {
+			return;
+		}
+		xChange *= mouseSensitivity;
+		yChange *= mouseSensitivity;
+
+		ProcessMouseMovement(xChange, yChange);
+	}
+
+	void ProcessMouseScroll(float yOffset)
+	{
+		if (FoVy >= 1.0f && FoVy <= 90.0f) {
+			FoVy -= yOffset;
+		}
+		if (FoVy <= 1.0f)
+			FoVy = 1.0f;
+		if (FoVy >= 90.0f)
+			FoVy = 90.0f;
+	}
+
+private:
+	void ProcessMouseMovement(float xOffset, float yOffset, bool constrainPitch = true)
+	{
+		yaw += xOffset;
+		pitch += yOffset;
+		if (constrainPitch) {
+			if (pitch > 89.0f)
+				pitch = 89.0f;
+			if (pitch < -89.0f)
+				pitch = -89.0f;
+		}
+		UpdateCameraVectors();
+	}
+
+	void UpdateCameraVectors()
+	{
+		this->forward.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+		this->forward.y = sin(glm::radians(pitch));
+		this->forward.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+		this->forward = glm::normalize(this->forward);
+		right = glm::normalize(glm::cross(forward, worldUp));
+		up = glm::normalize(glm::cross(right, forward));
+	}
+
+protected:
+	const float cameraSpeedFactor = 2.5f;
+	const float mouseSensitivity = 0.1f;
+
+	float zNear;
+	float zFar;
+	float FoVy;
+	int width;
+	int height;
+	bool isPerspective;
+
+	glm::vec3 position;
+	glm::vec3 forward;
+	glm::vec3 right;
+	glm::vec3 up;
+	glm::vec3 worldUp;
+
+	float yaw;
+	float pitch;
+
+	bool bFirstMouseMove = true;
+	float lastX = 0.f, lastY = 0.f;
+};
+GLuint ProjMatrixLocation, ViewMatrixLocation, WorldMatrixLocation;
+Camera* pCamera = nullptr;
+
+void Cleanup()
+{
+	delete pCamera;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+void processInput(GLFWwindow* window);
